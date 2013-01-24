89a2424e08dd39975aa3746cb5217521087666b7
Lov Why <rankun203@gmail.com>
2013-01-24 23:52:41 +0800

    Hibernate4支持 ，使用LocalSessionFactoryBean ，Spring管理事务之后，session不用beginTransaction() ，Dependencies的管理，注意不要同时有Hibernate3和Hibernate4

---
+++ Register/src/com/mindfine/ssh/register/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.mindfine.ssh.register.service.impl;
+
+import com.mindfine.ssh.register.dao.impl.IUserDAOImplHibernate;
+import com.mindfine.ssh.register.model.User;
+import com.mindfine.ssh.register.service.UserService;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rankun203
+ * Date: 13-1-9
+ * Time: 上午8:52
+ */
+//@Component("userService")
+public class UserServiceImpl implements UserService {
+    private IUserDAOImplHibernate userDAO;
+
+    @Override
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public boolean register(User user) {
+        if (user.getPassWord().equals(user.getRePass())) {
+            userDAO.insertUser(user);
+            return true;
+        }
+        return false;
+    }
+
+//    @Resource
+    public void setUserDAO(IUserDAOImplHibernate userDAO) {
+        this.userDAO = userDAO;
+    }
+
+}
